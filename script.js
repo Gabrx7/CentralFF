e22e53d6d11015b3e680adcd9fb4c5a6c011330f
Gabriel Rosinski <113986687+Gabrx7@users.noreply.github.com>
2022-12-15 11:39:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function EntrarGrupoWhatsApp(){
+    document.location.href = 'https://chat.whatsapp.com/Fw3eGB8em38KvfJIdqgqDH'
+}
+
+function EntrarGrupoWhatsAppVendas(){
+    document.location.href = 'https://chat.whatsapp.com/L3lEwElHMxhGy5R9FsHG9o'
+}
+
+function ChamarADM1(){
+    document.location.href = 'https://api.whatsapp.com/send?phone=5544997619331&text=Opa%20Gabriel,%20gostaria%20de...'
+}
+
+function EntrarServidorDC1() {
+    document.location.href = 'https://4br.me/aYwsGjo'
+}
+
+function PackSensi1() {
+    document.location.href = 'https://4br.me/alv3UPPxV'
+}
+
+function EntrarDiarioNZZ(){
+    document.location.href = 'https://chat.whatsapp.com/LwpyIqO405i4RgRdxdy2z8'
+}
+
+function EntrarComeia(){
+    document.location.href = 'https://chat.whatsapp.com/KKgWZNGmIoqKuLO8O0ygyf'
+}
